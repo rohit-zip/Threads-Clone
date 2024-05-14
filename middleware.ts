280f69990479290edc8957547100a2ee637565f0
Rohit <rohitparih@gmail.com>
2024-05-14 22:54:53 +0530

    feature/clerk-authentication

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import {authMiddleware} from "@clerk/nextjs/server";
+
+export default authMiddleware({
+    publicRoutes: [
+        '/',
+        '/api/webhook/clerk'
+    ],
+    ignoredRoutes: [
+        '/api/webhook/clerk'
+    ]
+});
+
+export const config = {
+    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
